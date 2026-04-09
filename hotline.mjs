c25963fca815123f08b3210fbb13639619b3e927
Arsen <arsen@mirzaev.sexy>
2026-04-09 09:41:42 +0500

    returned `default`

--- hotline.mjs
+++ hotline.mjs
@@ -21,7 +21,7 @@
  * @license http://www.wtfpl.net/ Do What The Fuck You Want To Public License
  * @author Arsen Mirzaev Tatyano-Muradovich <arsen@mirzaev.sexy>
  */
-export class hotline {
+export default class hotline {
 	/**
 	 * @name Shell
 	 *
